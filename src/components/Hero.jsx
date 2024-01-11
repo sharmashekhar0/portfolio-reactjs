2ef1bcedff6dde5a381837ecb5c3a8de426722c9
sharmashekhar0 <beingperfect76@gmail.com>
2024-01-11 21:58:07 +0530

    add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Hero() {
+	const dynamicHeight = `calc(100vh - 64px)`;
+
+	return (
+		<div style={{ height: dynamicHeight }} className={`text-[#F0E68C] p-20 w-screen`}>
+			<div className="w-2/3 h-full text-7xl flex items-center justify-center">
+				<div>
+					<p>Hey!</p>
+					<p>I'm</p>
+					<p className="font-semibold">Shekhar</p>
+					<p className="font-semibold">Sharma</p>
+					<p className="">Full Stack Developer</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Hero;
