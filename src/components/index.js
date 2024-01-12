11bd4206e0fbe5d2fb2cd1af80055760f0caaf1a
sharmashekhar0 <beingperfect76@gmail.com>
2024-01-12 15:22:01 +0530

    add index file

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Container from "./Container/container";
+import Header from "./Header/Header";
+import Hero from "./Hero";
+import Skills from "./Skills";
+import Academics from "./Academics";
+import Projects from "./Projects";
+import Footer from "./Footer/Footer";
+
+export { 
+	Container,
+	Header,
+	Hero,
+	Skills,
+	Academics,
+	Projects,
+	Footer
+};
