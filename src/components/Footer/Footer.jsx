addd1d88176d94832e7d673e262816c616c495e4
sharmashekhar0 <beingperfect76@gmail.com>
2024-01-12 15:19:56 +0530

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div className="flex items-center justify-center h-12 mt-10 shadow-edCustom  px-20">
+			<p className="text-white">
+				© Copyright 2024 - Developed by Shekhar Sharma. All right
+				reserved.
+			</p>
+		</div>
+	);
+}
+
+export default Footer;
