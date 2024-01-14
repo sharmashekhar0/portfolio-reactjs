63ac3cdc4af33de167a6c70e962415fa9e0aba7a
sharmashekhar0 <beingperfect76@gmail.com>
2024-01-14 19:17:11 +0530

    add contact form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function ContactForm() {
+	return (
+		<div id="contact" className="w-1/2 px-20">
+			<p className="text-4xl text-white">Contact Form.</p>
+
+			<div className="flex flex-col gap-4 px-10 py-4 text-xl">
+				<input
+					className="outline-none p-2 px-4 text-white h-12 rounded bg-slate-800"
+					type="text"
+					placeholder="Enter Full Name"
+				/>
+				<input
+					className="outline-none p-2 px-4 text-white h-12 rounded bg-slate-800"
+					type="email"
+					placeholder="Enter Your Email"
+				/>
+				<textarea
+					className="outline-none p-2 px-4 text-white rounded bg-slate-800 resize-none"
+					id=""
+					cols="20"
+					rows="8"
+					placeholder="Your Message"
+				></textarea>
+				<button className="bg-blue-700 text-white h-10 rounded">
+					Send Message
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default ContactForm;
